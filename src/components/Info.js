ec88c696ee36c74cb0448438572bd489a065c16e
Thanh Thien <tuthanhthien02@gmail.com>
2021-05-24 21:35:33 +0700

    add react-icons

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { GoRepo, GoGist } from "react-icons/go";
+// import { FiUsers, FiUserPlus } from "react-icons/fi";
 
 const Info = () => {
     return <h1>Info Component</h1>;
