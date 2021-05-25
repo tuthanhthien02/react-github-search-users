35352a8860169d81ddd388dd03e11f352433ad22
Thanh Thien <tuthanhthien02@gmail.com>
2021-05-25 20:03:16 +0700

    style for search conponent

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { MdSearch } from "react-icons/md";
 
 const Search = () => {
-    return <h1>Search Component</h1>;
+    return (
+        <section className="search section">
+            <div className="search__wrapper section-center">
+                <form action="">
+                    <div className="search__form-control">
+                        <MdSearch />
+                        <input type="text" placeholder="enter github user" />
+                        <button>Search</button>
+                    </div>
+                </form>
+                <h3>requests: 60 / 60</h3>
+            </div>
+        </section>
+    );
 };
 
 export default Search;
